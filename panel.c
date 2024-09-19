17c7cb5c72729fa7a7f67ae06c7b377c66e2f394
branchwag <branchleannelmb@gmail.com>
2024-09-19 19:46:31 -0400

    adds remaining buttons

--- panel.c
+++ panel.c
@@ -31,7 +31,19 @@ int main(void) {
 	  DrawRectangle(370, 530, 80, 80, BLACK); // 8 button
 	  DrawRectangle(460, 530, 80, 80, BLACK); // 9 button
 	  DrawRectangle(550, 530, 80, 80, BLACK); // CLR button
+
+	  DrawRectangle(190, 620, 80, 80, BLACK); // 1 button
+	  DrawRectangle(280, 620, 80, 80, BLACK); // 4 button
+	  DrawRectangle(370, 620, 80, 80, BLACK); // 5 button
+	  DrawRectangle(460, 620, 80, 80, BLACK); // 6 button
+	  DrawRectangle(550, 620, 80, 80, BLACK); // PRO button
 	
+	  DrawRectangle(190, 710, 80, 80, BLACK); // 0 button
+	  DrawRectangle(280, 710, 80, 80, BLACK); // 1 button
+	  DrawRectangle(370, 710, 80, 80, BLACK); // 2 button
+	  DrawRectangle(460, 710, 80, 80, BLACK); // 3 button
+	  DrawRectangle(550, 710, 80, 80, BLACK); // KEYREL button
+
 	  DrawRectangle(640, 550, 80, 80, BLACK); //ENTR button
 	  DrawRectangle(640, 640, 80, 80, BLACK); //RSET button
 	  EndDrawing();
